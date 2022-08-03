5f9cf973350aa137c069471dbee29324c06911db
Grzegorz Patyk <grzegorzxpatyk@gmail.com>
2022-08-03 18:06:06 +0200

    use new component to build basic UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import React, { useState } from 'react';
 import Button from './components/Button';
-import { darkTheme, globalStyles } from './styles/stitches';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import { darkTheme, globalStyles, styled } from './styles/stitches';
+import { Separator } from './components/Separator';
+import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
+
+const FlexContainer = styled('div', {
+    display: 'flex',
+    justifyContent: 'start',
+    alignItems: 'center',
+    flexDirection: 'row',
+});
 
 function App() {
     globalStyles();
@@ -12,10 +23,55 @@ function App() {
 
     return (
         <div id="App" className={isUsingDarkTheme ? darkTheme : ''}>
-            <h1>portfolio</h1>
-            <Button onClick={switchTheme}>
-                Switch to {isUsingDarkTheme ? 'light' : 'dark'} theme
-            </Button>
+            <Header>
+                <h1>portfolio</h1>
+                <Navbar>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Blog</li>
+                        <li>Contact</li>
+                    </ul>
+                </Navbar>
+                <Button onClick={switchTheme} css={{ padding: '.6rem' }}>
+                    {isUsingDarkTheme ? <SunIcon /> : <MoonIcon />}
+                </Button>
+            </Header>
+            <main
+                style={{
+                    margin: '4rem auto',
+                    width: '50vw',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <section>
+                    <h2 style={{ margin: '.5rem 0' }}>
+                        Welcome to my portfolio!
+                    </h2>
+                    <p>
+                        I'm Greg - a software engineer from Poland. Nice to meet
+                        you!
+                    </p>
+                    <Separator css={{ margin: '15px 0' }} />
+                    <FlexContainer css={{ height: '20px' }}>
+                        Github{' '}
+                        <Separator
+                            decorative
+                            orientation="vertical"
+                            css={{ margin: '0 15px' }}
+                        />
+                        LinkedIN
+                        <Separator
+                            decorative
+                            orientation="vertical"
+                            css={{ margin: '0 15px' }}
+                        />
+                        Contact
+                    </FlexContainer>
+                </section>
+            </main>
         </div>
     );
 }
