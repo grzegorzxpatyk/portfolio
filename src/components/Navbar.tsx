a5338c6f2b91eebfd5a26eafcb581fc1ac4254e1
Grzegorz Patyk <70432509+grzegorzxpatyk@users.noreply.github.com>
2022-08-09 15:04:49 +0200

    make navbar elemetns margin responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,23 @@
-import { styled } from '../styles/stitches';
+import { styled } from "../styles/stitches";
 
-const Navbar = styled('nav', {
-    '& ul': {
-        padding: 0,
-        margin: 0,
-        listStyle: 'none',
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        '& li': {
-            margin: '0 1rem',
-            '&:first-of-type': {
-                margin: '0 1rem 0 0',
-            },
-            '&:last-of-type': {
-                margin: '0 0 0 1rem',
-            },
-        },
-    },
+const Navbar = styled("nav", {
+  "& ul": {
+    padding: 0,
+    margin: 0,
+    listStyle: "none",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    "& li": {
+      margin: "0 min(1rem, 1vw)",
+      "&:first-of-type": {
+        margin: "0 min(1rem, 1vw) 0 0"
+      },
+      "&:last-of-type": {
+        margin: "0 0 0 min(1rem, 1vw)"
+      }
+    }
+  }
 });
 
 export default Navbar;
