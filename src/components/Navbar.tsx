db73e08d412d39a967f866557a9ddab129f1d605
Grzegorz Patyk <70432509+grzegorzxpatyk@users.noreply.github.com>
2022-08-09 15:08:56 +0200

    change prettier settings and format Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,20 @@
-import { styled } from "../styles/stitches";
+import { styled } from '../styles/stitches';
 
-const Navbar = styled("nav", {
-  "& ul": {
+const Navbar = styled('nav', {
+  '& ul': {
     padding: 0,
     margin: 0,
-    listStyle: "none",
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    "& li": {
-      margin: "0 min(1rem, 1vw)",
-      "&:first-of-type": {
-        margin: "0 min(1rem, 1vw) 0 0"
+    listStyle: 'none',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    '& li': {
+      margin: '0 min(1rem, 1vw)',
+      '&:first-of-type': {
+        margin: '0 min(1rem, 1vw) 0 0'
       },
-      "&:last-of-type": {
-        margin: "0 0 0 min(1rem, 1vw)"
+      '&:last-of-type': {
+        margin: '0 0 0 min(1rem, 1vw)'
       }
     }
   }
