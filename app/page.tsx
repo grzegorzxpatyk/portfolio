947d98af5eee48fd723e5847325249d53bb27541
grzegorzxpatyk <grzegorzxpatyk@gmail.com>
2024-03-29 00:11:25 +0100

    update bio description

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,15 @@
 export default function Home() {
     return (
-        <section className='w-full flex justify-start items-center'>
-            <article>
-                <h1 className='text-2xl text-white mb-8'>
-                    Hi! my name is Greg and{' '}
-                    <strong>this is my portfolio</strong> {';)'}
-                </h1>
-                <p className='text-base text-justify text-zinc-300'>
-                    I am dedicated and innovative Frontend Developer with a
-                    unique blend of skills derived from a background in
-                    architecture and passion for programming. Divergent thinker,
-                    infusing creativity and functionality into every project. My
-                    favorite aspect of web application development lies in
-                    crafting beautiful user interfaces, where I relish the
-                    opportunity to create aesthetic designs and implement
-                    captivating animations that elevate user experience to a new
-                    level. Passionate about design, architecture, snowboarding
-                    and dogs 🐶
-                </p>
-            </article>
+        <section className='w-full flex flex-col justify-start items-start'>
+            <h1 className='inline text-3xl text-white mb-8'>
+                Hello there! I am Greg 👋
+            </h1>
+            <p className='text-base text-justify text-zinc-300'>
+                a trained architect turned programmer. I specialize in
+                crafting user interfaces and implementing designs that enhance
+                web experiences. Outside of work, I enjoy woodworking, exploring
+                nature, and spending time with my dogs.
+            </p>
         </section>
     );
 }
