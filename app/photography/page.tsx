5669b1fae4b1583a75841c2e0226b1c11e515eb9
grzegorzxpatyk <grzegorzxpatyk@gmail.com>
2024-03-29 01:46:43 +0100

    implement wip /photography route

---
+++ app/photography/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <section className='w-full h-[50vh] flex flex-col justify-center items-center'>
+            <h2>Photography portfolio is coming soon 😏</h2>
+        </section>
+    );
+}
