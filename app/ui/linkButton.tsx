37b8ba324e4a47b0d5e080280d7103d9a03c5c80
grzegorzxpatyk <grzegorzxpatyk@gmail.com>
2024-03-31 23:27:29 +0200

    improve LinkButton component styling

--- app/ui/linkButton.tsx
+++ app/ui/linkButton.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import { ReactNode } from 'react';
+import { GeistMono } from 'geist/font/mono';
 
 export default function LinkButton({
     href,
@@ -11,14 +12,15 @@ export default function LinkButton({
     children: ReactNode;
 }) {
     return (
-        <Link
-            href={href}
-            className={
-                'py-1 px-2 my-2 text-base text-zinc-900 dark:text-zinc-200 bg-zinc-50 dark:bg-zinc-900 border border-zinc-400 dark:border-zinc-700 rounded inline-flex justify-center items-center' +
-                (className ? ` ${className}` : '')
-            }
-        >
-            {children}
+        <Link href={href} passHref>
+            <button
+                type='button'
+                className={`${GeistMono.className} py-1 px-2 my-2 text-base text-zinc-900 dark:text-zinc-200 bg-zinc-50 dark:bg-zinc-900 border border-zinc-400 dark:border-zinc-700 rounded inline-flex justify-center items-center ${
+                    className ?? ` ${className}`
+                }`}
+            >
+                {children}
+            </button>
         </Link>
     );
 }
