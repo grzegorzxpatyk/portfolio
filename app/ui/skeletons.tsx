18b2c9041cd524de7a380dfa54c69ae3d8a9a3c0
grzegorzxpatyk <grzegorzxpatyk@gmail.com>
2024-04-04 02:43:50 +0200

    fix projectsList skeleton

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -2,10 +2,10 @@ export function ProjectsSkeleton() {
     return (
         <ul className='flex flex-col justify-evenly items-start animate-pulse'>
             {[...Array(6)].map((el, i) => (
-                <li className='mb-1 h-fit' key={i}>
-                    <span className='block w-20 h-4 my-[6px] bg-zinc-500'></span>
-                    <span className='block w-48 h-3.5 my-[3px] bg-zinc-500'></span>
-                    <span className='w-fit flex flex-row gap-x-1 gap-y-0 h-3.5 my-[4px]'>
+                <li className='mb-4 h-fit' key={i}>
+                    <span className='block w-20 h-5 mt-[4px] mb-[8px] bg-zinc-500'></span>
+                    <span className='block w-48 h-4 mb-[7px] bg-zinc-500'></span>
+                    <span className='w-fit flex flex-row gap-x-1 gap-y-0 h-3.5 mb-[3px]'>
                         {[...Array(5)].map((el, j) => (
                             <span
                                 key={`${i}-${j}`}
