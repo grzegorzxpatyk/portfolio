8f62985d6381f0f2a29d5bc6a04a32feea3a556a
grzegorzxpatyk <grzegorzxpatyk@gmail.com>
2024-04-04 22:11:43 +0200

    implement 3d boxShadow in avatar component

--- app/ui/avatar.tsx
+++ app/ui/avatar.tsx
@@ -3,9 +3,11 @@
 import Image from 'next/image';
 import Portrait from '../lib/avatar.png';
 import { useRef } from 'react';
+import colors from 'tailwindcss/colors';
 
 export default function Avatar() {
     const avatarRef = useRef<HTMLImageElement | null>(null);
+
     function handleMouseMove(e: React.MouseEvent<HTMLImageElement>) {
         if (!avatarRef.current) return;
 
@@ -16,7 +18,6 @@ export default function Avatar() {
         const yVal = e.clientY - avatarRect.y;
 
         const yRotation = 20 * ((xVal - width / 2) / width);
-
         const xRotation = -20 * ((yVal - height / 2) / height);
 
         const cssString =
@@ -27,12 +28,19 @@ export default function Avatar() {
             'deg)';
 
         avatarRef.current.style.transform = cssString;
+
+        const shadowX = (-xVal + 100) / 6;
+        const shadowY = (-yVal + 100) / 6;
+
+        avatarRef.current.style.boxShadow = `${shadowX}px ${shadowY}px 0px ${colors.blue[500]}66, 
+        ${shadowX * 2}px ${shadowY * 2}px 0px ${colors.red[500]}33`;
     }
 
     function handleMouseOut(e: React.MouseEvent<HTMLImageElement>) {
         if (!avatarRef.current) return;
         avatarRef.current.style.transform =
             'perspective(500px) scale(1) rotateX(0) rotateY(0)';
+        avatarRef.current.style.boxShadow = `none`;
     }
 
     return (
@@ -42,7 +50,7 @@ export default function Avatar() {
             width={200}
             height={200}
             alt='My portrait.'
-            className='hidden sm:block rounded-lg sm:row-span-2 transition-transform ease-linear'
+            className='hidden sm:block relative rounded-lg sm:row-span-2 transition-all duration-500 ease-in-out hover:transition-transform hover:duration-100 hover:ease-linear'
             placeholder='blur'
             onMouseMove={handleMouseMove}
             onMouseOut={handleMouseOut}
