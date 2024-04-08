1acd58c2b3a8686beef6d9e6bed60c758bc62801
grzegorzxpatyk <grzegorzxpatyk@gmail.com>
2024-04-08 23:28:26 +0200

    add animation on avatar component init

--- app/ui/avatar.tsx
+++ app/ui/avatar.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image';
 import Portrait from '../lib/avatar.png';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import colors from 'tailwindcss/colors';
 
 export default function Avatar() {
@@ -11,11 +11,20 @@ export default function Avatar() {
     function handleMouseMove(e: React.MouseEvent<HTMLImageElement>) {
         if (!avatarRef.current) return;
 
-        const height = avatarRef.current.height;
-        const width = avatarRef.current.width;
         const avatarRect = avatarRef.current.getBoundingClientRect();
         const xVal = e.clientX - avatarRect.x;
         const yVal = e.clientY - avatarRect.y;
+
+        setAvatarTrasform({ xVal, yVal });
+    }
+
+    function setAvatarTrasform({ xVal, yVal }: { [key: string]: number }) {
+        if (!avatarRef.current) return;
+
+        console.log(xVal, yVal);
+
+        const height = avatarRef.current.height;
+        const width = avatarRef.current.width;
 
         const yRotation = 20 * ((xVal - width / 2) / width);
         const xRotation = -20 * ((yVal - height / 2) / height);
@@ -32,16 +41,52 @@ export default function Avatar() {
         const shadowX = (-xVal + 100) / 6;
         const shadowY = (-yVal + 100) / 6;
 
-        avatarRef.current.style.boxShadow = `${shadowX}px ${shadowY}px 0px ${colors.blue[500]}66, 
+        avatarRef.current.style.boxShadow = `${shadowX}px ${shadowY}px 0px ${
+            colors.blue[500]
+        }66, 
         ${shadowX * 2}px ${shadowY * 2}px 0px ${colors.red[500]}33`;
     }
 
     function handleMouseOut(e: React.MouseEvent<HTMLImageElement>) {
+        resetAvatarTransform();
+    }
+
+    function resetAvatarTransform() {
         if (!avatarRef.current) return;
         avatarRef.current.style.transform =
             'perspective(500px) scale(1) rotateX(0) rotateY(0)';
         avatarRef.current.style.boxShadow = `none`;
     }
+
+    useEffect(() => {
+        setTimeout(() => {}, 1000);
+        let counter = 0;
+        const avatarAnimation = setInterval(() => {
+            if (counter >= 3) {
+                resetAvatarTransform();
+                clearInterval(avatarAnimation);
+            } else {
+                switch (counter) {
+                    case 0:
+                        setAvatarTrasform({ xVal: 25, yVal: 18 });
+                        break;
+                    case 1:
+                        setAvatarTrasform({ xVal: 180, yVal: 15 });
+                        break;
+                    case 2:
+                        setAvatarTrasform({ xVal: 23, yVal: 191 });
+                        break;
+                    default:
+                        resetAvatarTransform();
+                        break;
+                }
+                counter++;
+            }
+        }, 1500);
+        return () => {
+            clearInterval(avatarAnimation);
+        };
+    }, []);
 
     return (
         <Image
