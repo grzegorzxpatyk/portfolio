5430204f4ea860c70050527d1c53a52f572a043c
grzegorzxpatyk <grzegorzxpatyk@gmail.com>
2024-04-21 00:30:27 +0200

    refactor: use target prop for LinkButton comp

--- app/ui/linkButton.tsx
+++ app/ui/linkButton.tsx
@@ -5,16 +5,18 @@ type LinkButtonProps = Omit<React.JSX.IntrinsicElements['button'], 'type'> & {
     href: string;
     className?: string;
     children: React.ReactNode;
+    target?: React.HTMLAttributeAnchorTarget;
 };
 
 export default function LinkButton({
     href,
     className = '',
     children,
+    target = '_self',
     ...props
 }: LinkButtonProps) {
     return (
-        <Link href={href} passHref>
+        <Link href={href} target={target} passHref>
             <Button className={className} {...props}>
                 {children}
             </Button>
