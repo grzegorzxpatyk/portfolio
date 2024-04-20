35672142ebdd58daa626d0e27efe2a6f2afc4c75
grzegorzxpatyk <grzegorzxpatyk@gmail.com>
2024-04-21 00:29:02 +0200

    refactor: make button component use className prop correctly

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -1,4 +1,6 @@
-type ButtonProps = Omit<React.JSX.IntrinsicElements['button'], 'type'> & {
+import clsx from 'clsx';
+
+type ButtonProps = React.JSX.IntrinsicElements['button'] & {
     className?: string;
     children: React.ReactNode;
 };
@@ -6,11 +8,10 @@ type ButtonProps = Omit<React.JSX.IntrinsicElements['button'], 'type'> & {
 export default function Button({ className, children, ...props }: ButtonProps) {
     return (
         <button
-            type='button'
-            className={
-                'inline-flex justify-center items-center py-1 px-2 my-2 text-base font-medium text-zinc-900 dark:text-zinc-200 bg-zinc-50 dark:bg-zinc-900 shadow hover:bg-zinc-200 dark:hover:bg-zinc-800 border border-zinc-400 dark:border-zinc-700 rounded active:scale-95 transition-transform' +
+            className={clsx(
+                'inline-flex items-center justify-center rounded border border-zinc-400 bg-zinc-50 px-2 py-1 text-base font-medium text-zinc-900 shadow transition-transform hover:bg-zinc-200 active:scale-95 dark:border-zinc-700 dark:bg-zinc-900 dark:text-zinc-200 dark:hover:bg-zinc-800',
                 className
-            }
+            )}
             {...props}
         >
             {children}
