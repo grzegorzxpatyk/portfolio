37e758c6a5f492a3f787e5197e1d2a3639ca42a6
grzegorzxpatyk <grzegorzxpatyk@gmail.com>
2024-05-15 00:31:46 +0200

    fix styling properly using react-aria hover and pressed states

--- components/button.tsx
+++ components/button.tsx
@@ -7,7 +7,7 @@ export default function Button({ className, children, ...props }: ButtonProps) {
     return (
         <ReactAriaButton
             className={clsx(
-                'inline-flex items-center justify-center rounded border border-zinc-400 bg-zinc-50 px-2 py-1 text-base font-medium text-zinc-900 shadow-lg shadow-zinc-600/20 transition-all hover:bg-zinc-200 hover:shadow-none active:scale-95 dark:border-zinc-700 dark:bg-zinc-900 dark:text-zinc-200 dark:hover:bg-zinc-800',
+                'inline-flex items-center justify-center rounded border border-zinc-400 bg-zinc-50 px-2 py-1 text-base font-medium text-zinc-900 shadow-lg shadow-zinc-600/20 transition-all hover:bg-zinc-200 hover:shadow-none data-[pressed]:scale-95 data-[pressed]:bg-zinc-200 data-[pressed]:shadow-none dark:border-zinc-700 dark:bg-zinc-900 dark:text-zinc-200 dark:hover:bg-zinc-800 dark:data-[pressed]:bg-zinc-800 ',
                 className
             )}
             {...props}
