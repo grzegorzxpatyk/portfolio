c2c28675e54c2f40ffc70039c19be9c6ce966cc2
grzegorzxpatyk <grzegorzxpatyk@gmail.com>
2024-05-15 03:01:15 +0200

    finish ripple button implementation

--- components/customButton.tsx
+++ components/customButton.tsx
@@ -2,85 +2,123 @@
 
 import clsx from 'clsx';
 import { motion } from 'framer-motion';
-import { AriaButtonProps, useButton } from '@react-aria/button';
-import { useHover } from '@react-aria/interactions';
-import { MouseEvent, useEffect, useRef, useState } from 'react';
+import { AriaButtonProps } from '@react-aria/button';
+import { PressEvent } from '@react-aria/interactions';
+import { MouseEvent, useLayoutEffect, useState } from 'react';
+import { Button } from 'react-aria-components';
 
-export function CustomButton(
-    props: AriaButtonProps & React.AllHTMLAttributes<HTMLButtonElement>
-) {
-    const ref = useRef(null);
-    const { buttonProps, isPressed } = useButton(props, ref);
-    const { isHovered, hoverProps } = useHover(props);
-    const { children } = props;
-    const className = props?.className;
-
-    const [coords, setCoords] = useState({ x: -1, y: -1 });
-    const [isRippling, setIsRippling] = useState(false);
-
-    useEffect(() => {
-        let timeout: ReturnType<typeof setTimeout>;
-        if (coords.x !== -1 && coords.y !== -1) {
-            setIsRippling(true);
-            timeout = setTimeout(() => setIsRippling(false), 600);
-        } else {
-            setIsRippling(false);
-        }
-        return () => {
-            clearTimeout(timeout);
-        };
-    }, [coords]);
-
-    function handleClick(event: MouseEvent<HTMLButtonElement>) {
-        event.preventDefault();
-        const { left, top } = event.currentTarget.getBoundingClientRect();
-
-        setCoords({
-            x: event.clientX - left - 24,
-            y: event.clientY - top - 24,
-        });
+export default function CustomButton({
+    children,
+    className,
+    ...props
+}: AriaButtonProps & { className?: string }) {
+    function handlePress(event: PressEvent) {
+        event.target;
     }
 
     return (
-        <button
-            ref={ref}
-            {...buttonProps}
-            {...hoverProps}
+        <Button
             className={clsx(
-                'relative inline-flex w-fit select-none items-center justify-center overflow-hidden rounded border border-zinc-400 px-6 py-3 text-base font-medium text-zinc-900 shadow-lg shadow-zinc-600/20 transition-all dark:border-zinc-700 dark:text-zinc-200',
-
-                (isPressed || (isPressed && isHovered)) &&
-                    'scale-95 bg-zinc-300 shadow-none dark:bg-zinc-700',
-                isHovered &&
-                    !isPressed &&
-                    'bg-zinc-200 shadow-none dark:bg-zinc-800',
-                !isHovered && !isPressed && 'bg-zinc-50 dark:bg-zinc-900',
+                'relative overflow-hidden rounded border border-zinc-400 bg-zinc-50 px-2 py-1 text-base font-medium text-zinc-900 shadow-lg shadow-zinc-600/20 transition-all data-[hovered]:bg-zinc-200 data-[hovered]:shadow-none data-[pressed]:shadow-none dark:border-zinc-700 dark:bg-zinc-900 dark:text-zinc-200 dark:data-[hovered]:bg-zinc-800',
                 className
             )}
-            onClick={handleClick}
+            onPress={handlePress}
+            {...props}
         >
             {children}
-            {isRippling && <Ripple coords={coords} />}
-        </button>
+            <RippleContainer duration={1000} />
+        </Button>
     );
 }
 
-export function Ripple({ coords }: { coords: { x: number; y: number } }) {
+export function Ripple({
+    coords,
+    duration,
+}: {
+    coords: { x: number; y: number; size: number };
+    duration: number;
+}) {
     return (
         <motion.span
-            className='absolute h-12 w-12 rounded-full bg-red-700/50 content-[_]'
-            style={{ left: coords.x, top: coords.y }}
-            initial={{ scale: 1, opacity: 1 }}
+            className='absolute rounded-full bg-yellow-400/50 content-[_]'
+            style={{
+                left: coords.x,
+                top: coords.y,
+                width: coords.size,
+                height: coords.size,
+            }}
+            initial={{ scale: 0, opacity: 1 }}
             animate={{
-                scale: 5,
+                scale: 2,
                 opacity: 0,
                 transition: {
-                    duration: 0.6,
-                    ease: 'backInOut',
+                    duration: duration / 1000,
+                    ease: 'easeInOut',
                 },
             }}
         ></motion.span>
     );
 }
 
-function RippleContainer() {}
+function RippleContainer({ duration }: { duration: number }) {
+    const [rippleArray, setRippleArray] = useState<
+        { x: number; y: number; size: number }[]
+    >([]);
+
+    useDebouncedRippleCleanUp(rippleArray.length, duration, () => {
+        setRippleArray([]);
+    });
+
+    const addRipple = (event: MouseEvent<HTMLDivElement>) => {
+        const { width, height, left, top } =
+            event.currentTarget.getBoundingClientRect();
+        const size = width > height ? width : height;
+        const x = event.pageX - left - size / 2;
+        const y = event.pageY - top - size / 2;
+        const newRipple = {
+            x,
+            y,
+            size,
+        };
+
+        console.log({ ...newRipple }, event.clientX, event.clientY);
+
+        setRippleArray([...rippleArray, newRipple]);
+    };
+
+    return (
+        <div
+            className='absolute bottom-0 left-0 right-0 top-0 h-full w-full'
+            onClick={addRipple}
+        >
+            {rippleArray.length > 0 &&
+                rippleArray.map((ripple, index) => (
+                    <Ripple
+                        key={`span-${index}`}
+                        coords={ripple}
+                        duration={duration}
+                    />
+                ))}
+        </div>
+    );
+}
+
+const useDebouncedRippleCleanUp = (
+    rippleCount: number,
+    duration: number,
+    cleanUpFunction: () => void
+) => {
+    useLayoutEffect(() => {
+        let bounce: ReturnType<typeof setTimeout> | null = null;
+        if (rippleCount > 0) {
+            clearTimeout(bounce!);
+
+            bounce = setTimeout(() => {
+                cleanUpFunction();
+                clearTimeout(bounce!);
+            }, duration * 4);
+        }
+
+        return () => clearTimeout(bounce!);
+    }, [rippleCount, duration, cleanUpFunction]);
+};
